92a9c2b52c4338b29b5f72eed1e6f4fdff5e7eca
ZhaoWenDao <zcw141180159@163.com>
2020-04-08 20:17:29 +0800

    ...

--- src/MatrixEigenvalueCalculation/Main.java
+++ src/MatrixEigenvalueCalculation/Main.java
@@ -20,18 +20,6 @@ public class Main {
                 eigenvalue.getMatrix().mtimes(value.eigenvector));
         System.out.println("lambda * vector为\n" +
                 value.eigenvector.times(value.eigenvalue));
-
-
-//        double[][] arr = {
-//                {1,1,0.5},{1,1,0.25},{0.5,0.25,2}
-//        };
-//        Eigenvalue eigenvalue = new Eigenvalue(arr);
-//        Eigenvalue.Value a = eigenvalue.PowerMethod();
-//        double lambda = a.eigenvalue;
-//        Matrix vector = a.eigenvector;
-//        System.out.println(lambda + "\n");
-//        System.out.println(vector);
-//        System.out.println("A * vector = \n" + eigenvalue.getMatrix().mtimes(vector));
-//        System.out.println("lambda * vector = \n" + vector.times(lambda));
+        
     }
 }
